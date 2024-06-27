d8a40806f541057dd68e5ebabd4fe76ea65424b3
Groot26 <saimorajkar123@gmail.com>
2024-06-28 04:20:56 +0530

    minor chnages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,60 +13,10 @@ export default function Home() {
   const size = isHovered ? 400 : 40;
 
   return (
-    <main>
+    <main className={styles.main}>
       <section className="home section" id="home">
         <div className="container">
-          
-
-          <motion.div
-            className={styles.mask}
-            animate={{
-              WebkitMaskPosition: `${(x ?? 0) - size / 2}px ${
-                (y ?? 0) - size / 2
-              }px`,
-              WebkitMaskSize: `${size}px`,
-            }}
-            transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
-          >
-            <div className="intro"
-              onMouseEnter={() => {
-                setIsHovered(true);
-              }}
-              onMouseLeave={() => {
-                setIsHovered(false);
-              }}
-            >
-              <Image
-                src={batman}
-                className="shadow-dark"
-                loading="lazy"
-                alt="Batman"
-              />
-              <h1>Sai Morajkar</h1>
-              <p>I m Batman</p>
-              <div className="social-links">
-                <a href="https://facebook.com/sai._.morajkar" target="_blank">
-                  <i className="fa fa-facebook fa-lg" />
-                </a>
-                <a href="https://github.com/Groot26" target="_blank">
-                  <i className="fa fa-github fa-lg" />
-                </a>
-                <a href="https://instagram.com/sai._.morajkar" target="_blank">
-                  <i className="fa fa-instagram fa-lg" />
-                </a>
-                <a
-                  href="https://linkedin.com/in/sairaj-morajkar/"
-                  target="_blank"
-                >
-                  <i className="fa fa-linkedin fa-lg" />
-                </a>
-              </div>
-            </div>
-          </motion.div>
-
-          <div
-            className={styles.body}>
-          <div className="intro">   
+          <div className="intro">
             <Image
               src={proflie}
               className="shadow-dark"
@@ -74,7 +24,30 @@ export default function Home() {
               alt="Profile"
             />
             <h1>Sairaj Morajkar</h1>
-            <p>Software Developer</p>
+            <motion.div
+              className={styles.mask}
+              animate={{
+                WebkitMaskPosition: `${(x ?? 0) - size / 2}px ${
+                  (y ?? 0) - size / 2
+                }px`,
+                WebkitMaskSize: `${size}px`,
+              }}
+              transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+            >
+              <p
+                onMouseEnter={() => {
+                  setIsHovered(true);
+                }}
+                onMouseLeave={() => {
+                  setIsHovered(false);
+                }}
+              >
+                I&apos;m Batman
+              </p>
+            </motion.div>
+            <div className={styles.body}>
+              <p>Software Developer</p>
+            </div>
             <div className="social-links">
               <a href="https://facebook.com/sai._.morajkar" target="_blank">
                 <i className="fa fa-facebook fa-lg" />
@@ -92,7 +65,6 @@ export default function Home() {
                 <i className="fa fa-linkedin fa-lg" />
               </a>
             </div>
-          </div>
           </div>
         </div>
       </section>
