45469a4b254a69f70487eb0a62953a22758ec8ac
Groot26 <saimorajkar123@gmail.com>
2024-06-28 05:22:26 +0530

    tried something

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,60 +14,61 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <section className="home section" id="home">
-        <div className="container">
-          <div className="intro">
-            <Image
-              src={proflie}
-              className="shadow-dark"
-              loading="lazy"
-              alt="Profile"
-            />
-            <h1>Sairaj Morajkar</h1>
-            <motion.div
-              className={styles.mask}
-              animate={{
-                WebkitMaskPosition: `${(x ?? 0) - size / 2}px ${
-                  (y ?? 0) - size / 2
-                }px`,
-                WebkitMaskSize: `${size}px`,
+      <motion.div
+        className={styles.mask}
+        animate={{
+          WebkitMaskPosition: `${(x ?? 0) - size / 2}px ${
+            (y ?? 0) - size / 2
+          }px`,
+          WebkitMaskSize: `${size}px`,
+        }}
+        transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+      >
+        <section className="home section" id="home">
+          <div className="container">
+            <div
+              className="intro"
+              onMouseEnter={() => {
+                setIsHovered(true);
               }}
-              transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+              onMouseLeave={() => {
+                setIsHovered(false);
+              }}
             >
-              <p
-                onMouseEnter={() => {
-                  setIsHovered(true);
-                }}
-                onMouseLeave={() => {
-                  setIsHovered(false);
-                }}
-              >
-                I&apos;m Batman
-              </p>
-            </motion.div>
-            <div className={styles.body}>
+              <Image
+                src={proflie}
+                className="shadow-dark"
+                loading="lazy"
+                alt="Profile"
+              />
+              <h1>Sairaj Morajkar</h1>
+
+              <p>I&apos;m Batman</p>
+
+              {/* <div className={styles.body}>
               <p>Software Developer</p>
-            </div>
-            <div className="social-links">
-              <a href="https://facebook.com/sai._.morajkar" target="_blank">
-                <i className="fa fa-facebook fa-lg" />
-              </a>
-              <a href="https://github.com/Groot26" target="_blank">
-                <i className="fa fa-github fa-lg" />
-              </a>
-              <a href="https://instagram.com/sai._.morajkar" target="_blank">
-                <i className="fa fa-instagram fa-lg" />
-              </a>
-              <a
-                href="https://linkedin.com/in/sairaj-morajkar/"
-                target="_blank"
-              >
-                <i className="fa fa-linkedin fa-lg" />
-              </a>
+            </div> */}
+              <div className="social-links">
+                <a href="https://facebook.com/sai._.morajkar" target="_blank">
+                  <i className="fa fa-facebook fa-lg" />
+                </a>
+                <a href="https://github.com/Groot26" target="_blank">
+                  <i className="fa fa-github fa-lg" />
+                </a>
+                <a href="https://instagram.com/sai._.morajkar" target="_blank">
+                  <i className="fa fa-instagram fa-lg" />
+                </a>
+                <a
+                  href="https://linkedin.com/in/sairaj-morajkar/"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin fa-lg" />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </motion.div>
     </main>
   );
 }
