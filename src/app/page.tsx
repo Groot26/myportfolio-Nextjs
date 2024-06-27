59b87cf29bdd42982506e2d6ed32b4db73c1daa0
Groot26 <saimorajkar123@gmail.com>
2024-06-28 04:40:26 +0530

    almost there...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,29 +13,32 @@ export default function Home() {
   const size = isHovered ? 400 : 40;
 
   return (
-    <main>
-      <section className="home section" id="home">
-        <div className="container">
-          
+    <main className={styles.main}>
+      {/* <section className="home section" id="home">
+        <div className="container"> */}
 
-          <motion.div
-            className={styles.mask}
-            animate={{
-              WebkitMaskPosition: `${(x ?? 0) - size / 2}px ${
-                (y ?? 0) - size / 2
-              }px`,
-              WebkitMaskSize: `${size}px`,
-            }}
-            transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
-          >
-            <div className="intro"
-              onMouseEnter={() => {
-                setIsHovered(true);
-              }}
-              onMouseLeave={() => {
-                setIsHovered(false);
-              }}
-            >
+      <motion.div
+        className={styles.mask}
+        animate={{
+          WebkitMaskPosition: `${(x ?? 0) - size / 2}px ${
+            (y ?? 0) - size / 2
+          }px`,
+          WebkitMaskSize: `${size}px`,
+        }}
+        transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+      >
+        <section
+          className="home section"
+          id="home"
+          onMouseEnter={() => {
+            setIsHovered(true);
+          }}
+          onMouseLeave={() => {
+            setIsHovered(false);
+          }}
+        >
+          <div className="container">
+            <div className="intro">
               <Image
                 src={batman}
                 className="shadow-dark"
@@ -62,40 +65,45 @@ export default function Home() {
                 </a>
               </div>
             </div>
-          </motion.div>
+          </div>
+        </section>
+      </motion.div>
 
-          <div
-            className={styles.body}>
-          <div className="intro">   
-            <Image
-              src={proflie}
-              className="shadow-dark"
-              loading="lazy"
-              alt="Profile"
-            />
-            <h1>Sairaj Morajkar</h1>
-            <p>Software Developer</p>
-            <div className="social-links">
-              <a href="https://facebook.com/sai._.morajkar" target="_blank">
-                <i className="fa fa-facebook fa-lg" />
-              </a>
-              <a href="https://github.com/Groot26" target="_blank">
-                <i className="fa fa-github fa-lg" />
-              </a>
-              <a href="https://instagram.com/sai._.morajkar" target="_blank">
-                <i className="fa fa-instagram fa-lg" />
-              </a>
-              <a
-                href="https://linkedin.com/in/sairaj-morajkar/"
-                target="_blank"
-              >
-                <i className="fa fa-linkedin fa-lg" />
-              </a>
+      <div className={styles.body}>
+        <section className="home section" id="home">
+          <div className="container">
+            <div className="intro">
+              <Image
+                src={proflie}
+                className="shadow-dark"
+                loading="lazy"
+                alt="Profile"
+              />
+              <h1>Sairaj Morajkar</h1>
+              <p>Software Developer</p>
+              <div className="social-links">
+                <a href="https://facebook.com/sai._.morajkar" target="_blank">
+                  <i className="fa fa-facebook fa-lg" />
+                </a>
+                <a href="https://github.com/Groot26" target="_blank">
+                  <i className="fa fa-github fa-lg" />
+                </a>
+                <a href="https://instagram.com/sai._.morajkar" target="_blank">
+                  <i className="fa fa-instagram fa-lg" />
+                </a>
+                <a
+                  href="https://linkedin.com/in/sairaj-morajkar/"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin fa-lg" />
+                </a>
+              </div>
             </div>
           </div>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
+      {/* </div>
+      </section> */}
     </main>
   );
 }
